42a8579eeb1580ee9e55584c782c7d4c3dcac3e4
devlaka <lakshanwarunab@gmail.com>
2022-07-24 13:53:28 +0530

    setup basic page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
 const App = () => {
-  return <div>Hello!</div>;
+  const categories = [
+    {
+      id: 1,
+      title: "Laptops",
+    },
+    {
+      id: 2,
+      title: "Desktops",
+    },
+    {
+      id: 3,
+      title: "Memory",
+    },
+    {
+      id: 4,
+      title: "Storage",
+    },
+    {
+      id: 5,
+      title: "Graphic Cards",
+    },
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          <div className="background-image"></div>
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
