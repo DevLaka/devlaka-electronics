5cd475a10d3b4c4f8830a874fd786775a9154efa
devlaka <lakshanwarunab@gmail.com>
2022-07-24 17:48:22 +0530

    add dynamic css and images

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
-import './categories.styles.scss'
+import "./categories.styles.scss";
 
 const App = () => {
   const categories = [
     {
       id: 1,
       title: "Laptops",
+      imageUrl:
+        "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
     },
     {
       id: 2,
       title: "Desktops",
+      imageUrl:
+        "https://images.unsplash.com/photo-1619597455322-4fbbd820250a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     },
     {
       id: 3,
       title: "Memory",
+      imageUrl:
+        "https://images.unsplash.com/photo-1541029071515-84cc54f84dc5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     },
     {
       id: 4,
       title: "Storage",
+      imageUrl:
+        "https://images.unsplash.com/photo-1597852074816-d933c7d2b988?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     },
     {
       id: 5,
       title: "Graphic Cards",
+      imageUrl:
+        "https://images.unsplash.com/photo-1591488320449-011701bb6704?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     },
   ];
 
   return (
     <div className="categories-container">
-      {categories.map(({ title, id }) => (
+      {categories.map(({ title, id, imageUrl }) => (
         <div key={id} className="category-container">
-          <div className="background-image"></div>
+          <div
+            className="background-image"
+            // Applying dynamic css by passing a style object.
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          ></div>
           <div className="category-body-container">
             <h2>{title}</h2>
             <p>Shop now</p>
