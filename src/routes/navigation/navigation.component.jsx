d791985ecc6a8dcdb8ae8d2ef2094a820ca44554
devlaka <lakshanwarunab@gmail.com>
2022-07-25 11:14:45 +0530

    add navigation styling and logo

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,10 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
 
+// Importing SVG as components
+import { ReactComponent as Logo } from "../../assets/logo_icon.svg";
+import "./navigation.styles.scss";
+
 // Step 8
 // Define Navigation Component
 const Navigation = () => {
@@ -12,22 +16,21 @@ const Navigation = () => {
       <div className="navigation">
         {/* Step 12 */}
         <Link className="logo-container" to="/">
-          <div>Logo</div>
+          <Logo className="logo" />
         </Link>
         {/* Step 11 */}
         {/* Define Links */}
         {/* Link component behaves like a anchor tag */}
         {/* Whatever we wrap in a link will give the child node navigation functionality */}
-        <div className="links-container">
+        <div className="nav-links-container">
           {/* This link navigates to /shop */}
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
         </div>
-        <h1>Navigation</h1>
-        {/* Define where to render the nested route components */}
-        <Outlet />
       </div>
+      {/* Define where to render the nested route components */}
+      <Outlet />
     </Fragment>
   );
 };
