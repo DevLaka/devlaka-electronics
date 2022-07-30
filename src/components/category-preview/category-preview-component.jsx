b7c2accd11ffa851cf6e425f6142630122a71412
devlaka <lakshanwarunab@gmail.com>
2022-07-30 20:50:36 +0530

    add links to category preview

--- src/components/category-preview/category-preview-component.jsx
+++ src/components/category-preview/category-preview-component.jsx
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom";
 import ProductCard from "../product-card/product-card.component";
-import "./category-component.styles.scss";
+import "./category-preview-component.styles.scss";
 
 const CategoryPreview = ({ title, products }) => {
   return (
     <div className="category-preview-container">
       <h2>
-        <span className="title">{title.toUpperCase()}</span>
+        <Link to={title}>
+          <span className="title">{title.toUpperCase()}</span>
+        </Link>
       </h2>
       <div className="preview">
         {products &&
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -15,12 +15,15 @@ const Category = () => {
   }, [category, categoriesMap]);
 
   return (
-    <div className="category-container">
-      {products &&
-        products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-    </div>
+    <>
+      <h2 className="title category-title">{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </>
   );
 };
 
