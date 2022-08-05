03a648a1208add1356ba921260eb59903eed2b41
devlaka <lakshanwarunab@gmail.com>
2022-08-05 16:22:28 +0530

    add custom middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,31 @@
 // Topic: Setup redux.
 // Step 1
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 // Step 7
 import { rootReducer } from "./root-reducer";
+
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("currentState: ", store.getState());
+
+  next(action);
+
+  console.log("next state: ", store.getState());
+};
 
 // Step 8
 // Define middleware.
 // Middle wears are library helpers that run before an action hits the reducer.
 // Dispatch and action => Hit the middleWare => Hit the Reducer.
 // Specifically, logger middleware catch actions before they hit reducers and then log out the state.
-const middleWares = [logger];
+const middleWares = [loggerMiddleware];
 
 // Step 9
 // Middle-wares are like enhancers.
