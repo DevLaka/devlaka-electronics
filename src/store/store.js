2bdfb91ad2f3df520d95336b4133705f9a305cc0
devlaka <lakshanwarunab@gmail.com>
2022-08-06 12:08:36 +0530

    add redux dev tools integration

--- src/store/store.js
+++ src/store/store.js
@@ -16,7 +16,13 @@ const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
   Boolean
 );
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
   persistedReducer,
